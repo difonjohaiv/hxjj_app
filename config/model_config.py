dd4d066198ef0e8725431aea7557aea6f78455d9
snailshaw <snailshaw@126.com>
2024-03-16 19:37:31 +0800

    0

--- config/model_config.py
+++ config/model_config.py
@@ -2,7 +2,7 @@
 LLM_MODEL_DICT = {
     "qwen_bs": {
         "type": "qwen",
-        "name": "Tongyi-Finance-14B-Chat",
+        "name": "Tongyi-Finance-14B-Chat-Int4",
         "model_path": "/tcdata/models/Tongyi-Finance-14B-Chat",
         "lora_path": "models/ckpt_agent",
         "lora_2_path": "models/ckpt_sql"
